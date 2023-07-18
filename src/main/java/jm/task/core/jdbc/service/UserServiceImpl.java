4a27d9421828da9822bddfd0b40fee0c74aa2af4
bks <andreevbks@gmail.com>
2023-07-18 19:25:01 +0400

    Task1(hyber)

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,13 +2,18 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 public class UserServiceImpl implements UserService {
     UserDaoJDBCImpl us = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
         us.createUsersTable();
+
     }
 
     public void dropUsersTable() {
