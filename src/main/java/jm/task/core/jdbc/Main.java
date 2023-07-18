c84af80a463024584fc84df9ae063f53d0e4fff9
bks <andreevbks@gmail.com>
2023-07-18 18:07:50 +0400

    Task1(hyber)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import java.sql.*;
+import java.sql.Connection;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.postgresql.Driver;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+//        UserDaoJDBCImpl udao = new UserDaoJDBCImpl();
+//        udao.createUsersTable();
+//
+//        udao.saveUser("Vova","Korova", (byte) 13);
+//        udao.saveUser("Vova1","Korova1", (byte) 14);
+//        udao.saveUser("Vova2","Korova2", (byte) 15);
+//        udao.saveUser("Vova3","Korova3", (byte) 16);
+
+//        udao.getAllUsers();
+//
+//        udao.removeUserById(1);
+
+//        udao.cleanUsersTable();
+//        udao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    Class <Driver> driverClass = Driver.class;
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String username = "postgres";
+    private static final String pass = "123";
+    private Util(){}
+    public static Connection open (){
+        try {
+            return DriverManager.getConnection(url,username,pass);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    Configuration conf = new Configuration().addAnnotatedClass(User.class);
+    SessionFactory sf = conf.buildSessionFactory();
+    Session session = sf.getCurrentSession();
 }
